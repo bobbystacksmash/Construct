32d01e2d32a97660d5c35f0d78a51a16fbeb9e17
Bobby Stacksmash <33038262+bobbystacksmash@users.noreply.github.com>
2018-01-24 22:03:49 +0100

    Added basic VFS fix, though very hacky. Needs tidying up.

--- src/runtime/virtfs.js
+++ src/runtime/virtfs.js
@@ -1,7 +1,5 @@
-
-const path   = require("path");
-const Folder = require("../winapi/FolderObject");
-
+const path       = require("path");
+const Folder     = require("../winapi/FolderObject");
 
 function VirtualFileSystem(ctx) {
 
@@ -11,9 +9,12 @@ function VirtualFileSystem(ctx) {
     //       create file MACE times that match the runtime environment.
     //
     //
+
     this.epoch   = ctx.epoch   || ctx.date.getTime();
     this.emitter = ctx.emitter,
-    this.mft   = { "C:": {} };
+    this.volumes = { "c:": { mft: [], usn: {} } };
+
+    this.mft   = { "c:": {} };
 
     return this;
 }
@@ -29,40 +30,74 @@ VirtualFileSystem.prototype.create = function (new_file_or_folder_path, type, fi
         };
     }
 
-    let path_parts = path.parse(new_file_or_folder_path),
-        path_list  = path_parts.name.split("\\"),
-        filename   = path.basename(path_parts.base),
-        $MFT       = this.mft,
-        parent     = $MFT["C:"];
+    new_file_or_folder_path = new_file_or_folder_path.toLowerCase();
 
-    path_list.forEach((path_part, i, arr) => {
+    let path_parts   = new_file_or_folder_path.replace("/", "\\").split("\\"),
+        filename     = path.basename(new_file_or_folder_path),
+        volume_label = path_parts.shift(),
+        $MFT         = this.volumes[volume_label].mft,
+        this_dir     = $MFT;
 
-        let last_element = i === (arr.length - 1);
+    var exists = (nf) => this_dir.filter((d) => { d.label === nf.label && d.type === nf.type });
 
-        if (last_element && type === "file") {
-            path_part = `${path_part}$`;
-        }
+    path_parts.forEach((path_part, i, arr) => {
 
-        let mtime =
-            atime =
-            ctime =
-            etime = new Date(this.epoch).getTime();
+        console.log("MFT:");
+        console.log(JSON.stringify($MFT));
+        console.log("this_dir");
+        console.log(JSON.stringify(this_dir));
+        console.log("----");
 
-        let table_entry = {
-            mtime    : mtime,
-            atime    : atime,
-            ctime    : ctime,
-            etime    : etime,
+        console.log("")
+        console.log("Path part: ", path_part);
+
+        let last_element = (i === (arr.length - 1));
+
+        let new_file_or_folder = {
+            label    : path_part,
             type     : last_element ? type : "folder",
-            contents : file_contents || null,
-            children : {}
+            children : []
         };
 
-        if (!$MFT.hasOwnProperty(path_part)) {
+        // Does the label of the current path match any of the children?
+        this_dir.unshift(new_file_or_folder);
+        console.log("exists>>>");
+        console.log(exists(new_file_or_folder));
+
+        this_dir = this_dir[0].children
+
+        console.log("#### END OF LOOP ####\n");
+
+        /*if (!$MFT.hasOwnProperty(path_part)) {
+
             parent[path_part] = table_entry;
             parent = table_entry.children;
-        }
+        }*/
     });
+
+    ["users"].forEachkkkkkk
+
+    console.log("FINAL MFT:");
+    console.log(JSON.stringify($MFT, null, 1));
+}
+
+VirtualFileSystem.prototype.folder_exists = function (folderpath) {
+
+    console.log(folderpath);
+
+    let folderpath_parts = folderpath.split("\\");
+    
+    // FIXME: Windows is case-insensitive.
+    function find_folderpath (folderpath, mft) {
+        console.log(folderpath);
+    }
+
+    return find_folderpath(folderpath_parts, this.mft);
+}
+
+
+VirtualFileSystem.prototype.file_exists = function (filepath) {
+    let filepath_parts = filepath.split("\\");
 }
 
 module.exports = VirtualFileSystem;
