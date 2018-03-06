fa617037b00fae1fe5eeda5f74c5e0e4ef8aa938
bobbystacksmash <33038262+bobbystacksmash@users.noreply.github.com>
2018-03-06 19:41:58 +0100

    Added rough implementation of the ShellApplication API.

--- src/winapi/ShellApplication.js
+++ src/winapi/ShellApplication.js
@@ -1,65 +1,33 @@
 
+const Component = require("../Component");
 
-/*
- * https://msdn.microsoft.com/en-us/library/windows/desktop/bb774094(v=vs.85).aspx
- *
- *
- * METHODS
- * =======
- *
- * [ ] AddToRecent          https://msdn.microsoft.com/en-us/library/windows/desktop/gg537735(v=vs.85).aspx
- * [ ] BrowseForFolder      https://msdn.microsoft.com/en-us/library/windows/desktop/bb774065(v=vs.85).aspx
- * [ ] CanStartStopService  https://msdn.microsoft.com/en-us/library/windows/desktop/gg537736(v=vs.85).aspx
- * [ ] CascadeWindows       https://msdn.microsoft.com/en-us/library/windows/desktop/bb774067(v=vs.85).aspx
- * [ ] ControlPanelItem     https://msdn.microsoft.com/en-us/library/windows/desktop/bb774069(v=vs.85).aspx
- * [ ] EjectPC              https://msdn.microsoft.com/en-us/library/windows/desktop/bb774071(v=vs.85).aspx
- * [ ] Explore              https://msdn.microsoft.com/en-us/library/windows/desktop/bb774073(v=vs.85).aspx
- * [ ] ExplorerPolicy       https://msdn.microsoft.com/en-us/library/windows/desktop/gg537737(v=vs.85).aspx
- * [ ] FileRun              https://msdn.microsoft.com/en-us/library/windows/desktop/bb774075(v=vs.85).aspx
- * [ ] FindComputer         https://msdn.microsoft.com/en-us/library/windows/desktop/bb774077(v=vs.85).aspx
- * [ ] FindFiles            https://msdn.microsoft.com/en-us/library/windows/desktop/bb774079(v=vs.85).aspx
- * [ ] FindPrinter          https://msdn.microsoft.com/en-us/library/windows/desktop/gg537738(v=vs.85).aspx
- * [ ] GetSetting           https://msdn.microsoft.com/en-us/library/windows/desktop/gg537739(v=vs.85).aspx
- * [ ] GetSystemInformation https://msdn.microsoft.com/en-us/library/windows/desktop/gg537740(v=vs.85).aspx
- * [ ] Help                 https://msdn.microsoft.com/en-us/library/windows/desktop/bb776786(v=vs.85).aspx
- * [ ] IsRestricted         https://msdn.microsoft.com/en-us/library/windows/desktop/gg537741(v=vs.85).aspx
- * [ ] IsServiceRunning     https://msdn.microsoft.com/en-us/library/windows/desktop/gg537742(v=vs.85).aspx
- * [ ] MinimiseAll          https://msdn.microsoft.com/en-us/library/windows/desktop/bb774083(v=vs.85).aspx
- * [ ] NameSpace            https://msdn.microsoft.com/en-us/library/windows/desktop/bb774085(v=vs.85).aspx
- * [ ] Open                 https://msdn.microsoft.com/en-us/library/windows/desktop/bb774086(v=vs.85).aspx
- * [ ] RefreshMenu          https://msdn.microsoft.com/en-us/library/windows/desktop/bb774090(v=vs.85).aspx
- * [ ] SearchCommand        https://msdn.microsoft.com/en-us/library/windows/desktop/jj635751(v=vs.85).aspx
- * [ ] ServiceStart         https://msdn.microsoft.com/en-us/library/windows/desktop/gg537743(v=vs.85).aspx
- * [ ] ServiceStop          https://msdn.microsoft.com/en-us/library/windows/desktop/gg537744(v=vs.85).aspx
- * [ ] SetTime              https://msdn.microsoft.com/en-us/library/windows/desktop/bb774092(v=vs.85).aspx
- * [ ] ShellExecute         https://msdn.microsoft.com/en-us/library/windows/desktop/gg537745(v=vs.85).aspx
- * [ ] ShowBrowserBar       https://msdn.microsoft.com/en-us/library/windows/desktop/gg537746(v=vs.85).aspx
- * [ ] ShutDownWindows      https://msdn.microsoft.com/en-us/library/windows/desktop/bb774098(v=vs.85).aspx
- * [ ] Suspend              https://msdn.microsoft.com/en-us/library/windows/desktop/bb774100(v=vs.85).aspx
- * [ ] TileHorizontally     https://msdn.microsoft.com/en-us/library/windows/desktop/bb774102(v=vs.85).aspx
- * [ ] TileVertically       https://msdn.microsoft.com/en-us/library/windows/desktop/bb774104(v=vs.85).aspx
- * [ ] ToggleDesktop        https://msdn.microsoft.com/en-us/library/windows/desktop/gg537747(v=vs.85).aspx
- * [ ] TrayProperties       https://msdn.microsoft.com/en-us/library/windows/desktop/bb774105(v=vs.85).aspx
- * [ ] UndoMinimizeAll      https://msdn.microsoft.com/en-us/library/windows/desktop/bb774106(v=vs.85).aspx
- * [ ] Windows              https://msdn.microsoft.com/en-us/library/windows/desktop/bb774107(v=vs.85).aspx
- * [ ] WindowsSecurity      https://msdn.microsoft.com/en-us/library/windows/desktop/gg537748(v=vs.85).aspx
- * [ ] WindowSwitcher       https://msdn.microsoft.com/en-us/library/windows/desktop/gg537749(v=vs.85).aspx
- *
- * PROPERTIES
- * ==========
- *
- * [ ] Application https://msdn.microsoft.com/en-us/library/windows/desktop/bb774063(v=vs.85).aspx
- * [ ] Parent      https://msdn.microsoft.com/en-us/library/windows/desktop/bb774089(v=vs.85).aspx
- *
- */
-
-class ShellApplication extends Component {
+class JS_ShellApplication extends Component {
 
     constructor (context) {
 	super(context);
 	this.ee = this.context.emitter;
     }
 
+    //
+    // PROPERTIES
+    // ==========
+    //
+    get Application () {
+	this.ee.emit("!ERROR::NOT_IMPLEMENTED", "ShellApplication.Application");
+	return null;
+    }
+
+
+    get Parent () {
+	this.ee.emit("@ShellApplication.Parent", arguments);
+	return null;
+    }
+
+
+    //
+    // METHODS
+    // =======
+    //
 
     // Adds a file to the most recently used (MRU) list.
     //  https://msdn.microsoft.com/en-us/library/windows/desktop/gg537735(v=vs.85).aspx
@@ -74,7 +42,7 @@ class ShellApplication extends Component {
 
 
     BrowseForFolder () {
-	this.ee.emit("!ERROR::NOT_IMPLEMENTED" "ShellApplication.BrowseForFolder");
+	this.ee.emit("!ERROR::NOT_IMPLEMENTED", "ShellApplication.BrowseForFolder");
 	return true;
     }
 
@@ -132,41 +100,230 @@ class ShellApplication extends Component {
 
     GetSetting (setting) {
 	// https://msdn.microsoft.com/en-us/library/windows/desktop/gg537739(v=vs.85).aspx
-	this.ee.emit("!ERROR::NOT_IMPLEMENTED", "ShellApplication.GetSetting");
+	this.ee.emit("!ERROR::NOT_IMPLEMENTED", "ShellApplication.GetSetting", arguments);
     }
 
 
     GetSystemInformation (system_info_name) {
 	// https://msdn.microsoft.com/en-us/library/windows/desktop/gg537740(v=vs.85).aspx
-	this.ee.emit("!ERROR::NOT_IMPLEMENTED", "ShellApplication.GetSystemInformation");
+	this.ee.emit("!ERROR::NOT_IMPLEMENTED", "ShellApplication.GetSystemInformation", arguments);
     }
-	
-	
+
+
+    Help () {
+	this.ee.emit("@ShellApplication::Help", arguments);
+    }
+
     
-}
-
-
-
-
-/*
-module.exports = function ShellApplication (opts) {
-
-    let ee = opts.emitter;
-
-    function ShellExecute (file, params, dir, operation, show) {
-        ee.emit(events.WINAPI.ShellApplication.ShellExecute, {
-            file: file,
-            args: params,
-            dir: dir,
-            operation: operation,
-            show: show
-        });
+    IsRestricted (group, restriction) {
+	this.ee.emit("@ShellApplication::IsRestricted", arguments);
+	return true;
     }
 
-    let ShellApplication = { 
-        ShellExecute: ShellExecute
-    };
 
-    return proxify2(ShellApplication, "ShellApplication", opts);
+    IsServiceRunning (service_name) {
+
+	this.ee.emit("@ShellApplication::IsServiceRunning", arguments);
+	
+	let service_index = this.context.ENVIRONMENT.Services.findIndex((s) => s === service_name);
+	return (service_index > -1);
+    }
+
+
+    MinimizeAll () {
+	this.ee.emit("@ShellApplication::MinimizeAll", arguments);
+    }
+
+
+    NameSpace (dir) {
+	//https://msdn.microsoft.com/en-us/library/windows/desktop/bb774085(v=vs.85).aspx
+	this.ee.emit("!ERROR::NOT_IMPLEMENTED", "ShellApplication.NameSpace", arguments);
+    }
+
+    
+    Open (dir) {
+	//https://msdn.microsoft.com/en-us/library/windows/desktop/bb774085(v=vs.85).aspx
+	this.ee.emit("!ERROR::NOT_IMPLEMENTED", "ShellApplication.Open", arguments);
+    }
+
+
+    RefreshMenu () {
+	this.ee.emit("@ShellApplication::RefreshMenu", arguments);
+    }
+
+
+    SearchCommand () {
+	this.ee.emit("@ShellApplication::SearchCommand", arguments);
+    }
+
+    // MSDN: https://msdn.microsoft.com/en-us/library/windows/desktop/gg537743(v=vs.85).aspx
+    //
+    // Arguments
+    // =========
+    //   - service_name
+    //     A String that contains the name of the service.
+    //
+    //   - make_persistent
+    //     Set to true to have the service started automatically by the
+    //     service control manager during system startup. Set to false
+    //     to leave the service configuration unchanged.
+    ServiceStart (service_name, make_persistent) {
+	this.ee.emit("@ShellApplication::ServiceStart", arguments);
+	return true; // Of course, your new service will always be created. ;-)
+    }
+
+    // MSDN: https://msdn.microsoft.com/en-us/library/windows/desktop/gg537744(v=vs.85).aspx
+    //
+    // Arguments
+    // =========
+    //   - service_name
+    //   A String that contains the name of the service.
+    //
+    //   - make_persistent
+    //     Set to true to have the service started by the service
+    //     control manager when ServiceStart is called. To leave
+    //     the service configuration unchanged, set 'make_persistent'
+    //     to false.
+    ServiceStop (service_name, make_persistent) {
+	this.ee.emit("@ShellApplication::ServiceStop", arguments);
+	return true;
+    }
+
+
+    SetTime () {
+	this.ee.emit("@ShellApplication::SetTime", arguments);
+    }
+
+
+    // MSDN: https://msdn.microsoft.com/en-us/library/windows/desktop/gg537745(v=vs.85).aspx
+    //
+    // SYNOPSIS
+    // ========
+    //
+    // This method is equivalent to launching one of the commands
+    // associated with a file's shortcut menu. Each command is
+    // represented by a verb string. The set of supported verbs varies
+    // from file to file. The most commonly supported verb is "open",
+    // which is also usually the default verb. Other verbs might be
+    // supported by only certain types of files.
+    //
+    // Arguments
+    // =========
+    //
+    //   - file
+    //     A String that contains the name of the file on which
+    //     ShellExecute will perform the action specified by
+    //     `operation'.
+    //
+    //   - args      [optional]
+    //     A string that contains parameter values for the operation.
+    //
+    //   - dir       [optional]
+    //     The fully qualified path of the directory that contains
+    //     the file specified by `file'.  If this parameter is not
+    //     specified, the current working directory is used.
+    //
+    //   - operation [optional]
+    //     The operation to be performed. This value is set to one
+    //     of the verb strings that is supported by the file.  If
+    //     this parameter is not specified, the default operation
+    //     is performed.
+    //
+    //   - show      [optional]
+    //
+    //     A recommendation as to how the application window should be
+    //     displayed initially. The application can ignore this
+    //     recommendation. This parameter can be one of the following
+    //     values. If this parameter is not specified, the application
+    //     uses its default value.
+    //
+    // USAGE
+    // =====
+    //
+    //   var objShell = new ActiveXObject("shell.application");
+    //   objShell.ShellExecute("notepad.exe", "", "", "open", 1);
+    //
+    ShellExecute (file, args, dir, operation, show) {
+	this.ee.emit("@ShellApplication::ShellExecute", arguments);
+    }
+
+    // MSDN: https://msdn.microsoft.com/en-us/library/windows/desktop/gg537746(v=vs.85).aspx
+    //
+    // SYNOPSIS
+    // ========
+    //
+    // Displays a browser bar.
+    //
+    // ARGUMENTS
+    // =========
+    //
+    //  - clsid
+    //    A String that contains the string form of the CLSID of the
+    //    browser bar to be displayed. The object must be registered
+    //    as an Explorer Bar object with a CATID_InfoBand component
+    //    category.
+    //
+    //  - show
+    //    Set to true to show the browser bar or false to hide it.
+    //
+    // USAGE
+    // =====
+    //
+    //  var objShell = new ActiveXObject("shell.application");
+    //  objShell.ShowBrowserBar("{EFA24E61-B078-11d0-89E4-00C04FC9E26E}", true);
+    //
+    ShowBrowserBar () {
+	this.ee.emit("@ShellApplication::ShowBrowserBar", arguments);
+    }
+
+
+    ShutdownWindows () {
+	this.ee.emit("@ShellApplication::ShutdownWindows", arguments);
+    }
+
+
+    TileHorizontally () {
+	this.ee.emit("@ShellApplication::TileHorizontally", arguments);
+    }
+
+    
+    TileVertically () {
+	this.ee.emit("@ShellApplication::TileVertically", arguments);
+    }
+
+
+    ToggleDesktop () {
+	this.ee.emit("@ShellApplication::ToggleDesktop", arguments);
+    }
+
+
+    TrayProperties () {
+	this.ee.emit("@ShellApplication::TrayProperties", arguments);
+    }
+
+
+    UndoMinimizeALL () {
+	this.ee.emit("@ShellApplication::UndoMinimizeALL", arguments);
+    }
+
+
+    Windows () {
+	this.ee.emit("!ERROR::NOT_IMPLEMENTED", "ShellApplication.Windows", arguments);
+    }
+
+
+    WindowsSecurity () {
+	this.ee.emit("!ERROR::NOT_IMPLEMENTED", "ShellApplication.WindowsSecurity", arguments);
+    }
+
+
+    WindowSwitcher () {
+	this.ee.emit("@ShellApplication::WindowSwitcher", arguments);	
+    }
 }
-*/
+
+
+module.exports = function create(context) {
+    let shell_application = new JS_ShellApplication(context);
+    return shell_application;
+};
