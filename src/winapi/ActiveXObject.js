5466abd213db904b41117b1d499d8d6a79e5af24
bobbystacksmash <33038262+bobbystacksmash@users.noreply.github.com>
2018-03-13 19:51:49 +0100

    Reworking the ActiveX module.

--- src/winapi/ActiveXObject.js
+++ src/winapi/ActiveXObject.js
@@ -3,8 +3,10 @@
  */
 
 const Component = require("../Component");
+const proxify   = require("../proxify2");
 
-//const JScript_WScript          = require("../winapi/WScript");
+
+const JScript_MSXML2XMLHTTP    = require("../winapi/MSXML2_XMLHTTP");
 const JScript_WshShell         = require("../winapi/WshShell");
 const JScript_ShellApplication = require("../winapi/ShellApplication");
 
@@ -31,9 +33,13 @@ class JS_ActiveXObject extends Component {
 
 	case "wscript.shell":
 	    this.ee.emit("@ActiveXObject::new::WScript.Shell");
-
 	    let wsh = new JScript_WshShell(context);
 	    return wsh;
+
+	case "msxml2.xmlhttp":
+	    this.ee.emit("@ActiveXObject::new::MSXML2.XMLHTTP");
+	    let xhr = new JScript_MSXML2XMLHTTP(context);
+	    return xhr;
 
 
 	default:
@@ -52,77 +58,5 @@ module.exports = function create (context) {
 	}
     };
 
-    return activex;
+    return proxify(context, activex);
 };
-
-
-
-
-
-
-/*
-var proxify2 = require("../proxify2");
-var evts     = require("../events");
-var WshShell = require("./WshShell");
-var ADODBStream = require("./ADODBStream");
-var ShellApplication = require("./ShellApplication");
-var FileSystemObject = require("./FileSystemObject");
-var XMLHttpRequest   = require("./XMLHttpRequest");
-
-function AActiveXObject (opts) {
-
-    var ee      = opts.emitter,
-        modules = opts.modules || {},
-        activex = proxify2({}, "ActiveXObject", { emitter: ee });
-
-    return function ActiveXObject (type) {
-
-        let orig_type = type;
-
-        type = orig_type.toLowerCase();
-
-        switch (type) { 
-
-            case "shell.application":
-                ee.emit(evts.WINAPI.ActiveXObject.new.Shell.Application, { type: orig_type });
-                var shellapplication = new ShellApplication({ emitter: ee });
-                return shellapplication;
-
-            case "wscript.shell":
-                ee.emit(evts.WINAPI.ActiveXObject.new.WScript.Shell, { type: orig_type });
-                var wshshell = new WshShell({ emitter: ee });
-                return wshshell;
-
-            case "scripting.filesystemobject":
-                ee.emit(evts.WINAPI.ActiveXObject.new.Scripting.FileSystemObject, { type: orig_type });
-                var fso = new FileSystemObject({ emitter: ee });
-                return fso;
-
-            case "adodb.stream":
-                ee.emit(evts.WINAPI.ActiveXObject.new.ADODB.Stream, { type: orig_type });
-                var adodbstream = new ADODBStream({ emitter: ee });
-                return adodbstream;
-
-            case "msxml2.serverxmlhttp":
-                ee.emit(evts.WINAPI.ActiveXObject.new.MSXML2.ServerXMLHttp, { type: orig_type });
-                var xhr = new XMLHttpRequest({ emitter: ee });
-                return xhr;
-
-            case "msxml2.xmlhttp":
-                ee.emit(evts.WINAPI.ActiveXObject.new.MSXML2.XMLHttp, { type: orig_type });
-                var xhr = new XMLHttpRequest({ emitter: ee });
-                return xhr;
-
-            default:
-                console.log("[ERROR] ActiveXObject does not know how to create a: " + type);
-                throw 42;
-                break;
-        }
-
-        return {};
-    };
-}
-
-
-module.exports = ActiveXObject;
-*/
