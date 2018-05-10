3cfb4e232d13d3a271431f54479bdc18a2369d7a
bobbystacksmash <33038262+bobbystacksmash@users.noreply.github.com>
2018-05-10 04:09:00 +0000

    Adding new textstream test.

---
+++ test/WINAPI/TextStream_test.js
@@ -0,0 +1,8 @@
+ const assert = require("chai").assert;
+const TextStream = require("../../src/winapi/support/TextStream");
+const VirtualFileSystem = require("../../src/runtime/virtfs");
+const iconv = require("iconv-lite");
+
+describe("TextStream", () => {
+
+});
