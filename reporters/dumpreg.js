da1b29b6279e2a74a0ecfb867ffb43447c3d9c18
bobbystacksmash <33038262+bobbystacksmash@users.noreply.github.com>
2018-08-14 04:07:51 +0100

    Added missing quote to dumpreg output reporter.

--- reporters/dumpreg.js
+++ reporters/dumpreg.js
@@ -10,7 +10,7 @@ module.exports = {
         events.forEach(event => {
 
             if (/wshshell/i.test(event.target) && /regread/i.test(event.prop)) {
-                console.log(`"${event.target}","${event.prop},"${event.args[0]}","${event.return}"`);
+                console.log(`"${event.target}","${event.prop}","${event.args[0]}","${event.return}"`);
             }
             else if (/wshshell/i.test(event.target) && /regwrite/i.test(event.prop)) {
                 console.log("REGWRITE");
