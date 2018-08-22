54a32652ef5bab2aa9e8dc0dad91289e8ea38761
bobbystacksmash <33038262+bobbystacksmash@users.noreply.github.com>
2018-08-22 04:44:44 +0100

    Removed old emitter calls from Date.

--- src/winapi/Date.js
+++ src/winapi/Date.js
@@ -2,11 +2,13 @@
 const Component = require("../Component"),
       proxify   = require("../proxify2");
 
+const DATE = Date;
+
 class JS_Date extends Component {
 
     constructor (context, dtstr) {
 	super(context, "Date");
-	this.date = new Date(this.context.epoch, dtstr);
+	this.date = new DATE(this.context.epoch);
 	this.ee   = this.context.emitter;
     }
 
@@ -17,78 +19,36 @@ class JS_Date extends Component {
 
 
     getdate () {
-
 	let dt = this.date.getDate();
-
-	this.ee.emit("@Date::getDate", {
-	    fn: "getDate",
-	    v: dt
-	});
-
 	return dt;
     }
 
 
     getmonth () {
-
 	let dt = this.date.getMonth();
-
-	this.ee.emit("@Date::getMonth", {
-	    fn: "getMonth",
-	    v: dt
-	});
-
 	return dt;
     }
 
 
     getyear () {
-
 	let dt = this.date.getYear() + 1900;
-
-	this.ee.emit("@Date::getYear", {
-	    fn: "getYear",
-	    v: dt
-	});
-
 	return dt;
     }
 
 
     gethours () {
-
 	let dt = this.date.getHours();
-
-	this.ee.emit("@Date::getHours", {
-	    fn: "getHours",
-	    v: dt
-	});
-
 	return dt;
     }
 
     getminutes () {
-
 	let dt = this.date.getMinutes();
-
-	this.ee.emit("@Date::getMinutes", {
-	    fn: "getMinutes",
-	    v: dt
-	});
-
 	return dt;
     }
 
 
     getseconds () {
-
 	let dt = this.date.getSeconds();
-
-	this.ee.emit("@Date::getSeconds", {
-	    fn: "getSeconds",
-	    v: dt
-	});
-
 	return dt;
     }
 
@@ -96,12 +56,6 @@ class JS_Date extends Component {
     gettime () {
 
 	let dt = this.date.getTime();
-
-	this.ee.emit("@Date::getTime", {
-	    fn: "getTime",
-	    v: dt
-	});
-
 	return dt;
     }
 
