840c0aeb3b576829d6af0da188d0fe9b55a8f8f3
bobbystacksmash <33038262+bobbystacksmash@users.noreply.github.com>
2018-09-08 09:55:16 +0100

    Fixed the case where the FS is interacted with via the ADODB.Stream -> savetofile method.

--- reporters/dumpfs.js
+++ reporters/dumpfs.js
@@ -14,8 +14,10 @@ module.exports = {
             console.log(JSON.stringify(events));
         }
         else if (event.meta && event.meta === "runtime.api.call") {
-
             if (/^(?:filesystemobject|file|folder)/i.test(event.target)) {
+                events.push(event);
+            }
+            else if (/^adodbstream$/i.test(event.target) && event.property.normalised === "savetofile") {
                 events.push(event);
             }
         }
