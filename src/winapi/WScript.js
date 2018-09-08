39bb2a6ae9c10631c49c9efdc2ec86c6637a0597
bobbystacksmash <33038262+bobbystacksmash@users.noreply.github.com>
2018-09-08 09:43:53 +0100

    Removed some legacy emitters from the WScript object.

--- src/winapi/WScript.js
+++ src/winapi/WScript.js
@@ -36,8 +36,6 @@ class JS_WScript extends Component {
 
     constructor (context) {
 	super(context, "WScript");
-        this.context = context;
-	this.ee = this.context.emitter;
     }
 
     //
@@ -51,14 +49,12 @@ class JS_WScript extends Component {
     // WScript.Arguments: https://msdn.microsoft.com/en-us/library/z2b05k8s(v=vs.84).aspx
     get arguments () {
 	let args = this.context.ENVIRONMENT.Arguments;
-	this.ee.emit("@WScript::Arguments", args);
 	return args;
     }
 
     // WScript.BuildVersion      https://msdn.microsoft.com/en-us/library/kt8ycte6(v=vs.84).aspx
     get buildversion () {
 	let build_version = this.context.ENVIRONMENT.BuildVersion;
-	this.ee.emit("@WScript::BuildVersion", build_version);
 	return build_version;
     }
 
@@ -74,7 +70,6 @@ class JS_WScript extends Component {
     // FullName https://msdn.microsoft.com/en-us/library/z00t383b(v=vs.84).aspx
     get fullname () {
 	let full_name = this.context.ENVIRONMENT.FullName;
-	this.ee.emit("@WScript::FullName", full_name);
 	return full_name;
     }
 
@@ -90,19 +85,16 @@ class JS_WScript extends Component {
     // Interactive https://msdn.microsoft.com/en-us/library/b48sxsw0(v=vs.84).aspx
     get interactive () {
 	let interactive = this.context.ENVIRONMENT.Interactive;
-	this.ee.emit("@WScript::Interactive", interactive);
 	return interactive;
     }
 
     set interactive (_) {
-        this.ee.emit("@WScript::Interactive", arguments);
     }
 
 
     // Name https://msdn.microsoft.com/en-us/library/3ktf76t1(v=vs.84).aspx
     get name () {
 	let name = this.context.ENVIRONMENT.Name;
-	this.ee.emit("@WScript::Name", name);
 	return name;
     }
 
@@ -121,7 +113,6 @@ class JS_WScript extends Component {
     // executable (CScript.exe or WScript.exe).
     get path () {
 	let path = this.context.ENVIRONMENT.Path;
-	this.ee.emit("@WScript::Path", path);
 	return path;
     }
 
@@ -136,7 +127,7 @@ class JS_WScript extends Component {
 
     // ScriptFullName    https://msdn.microsoft.com/en-us/library/cc5ywscw(v=vs.84).aspx
     get scriptfullname () {
-	let script_full_name = this.context.ENVIRONMENT.ScriptFullName;
+	let script_full_name = this.context.get_env("ScriptFullName");
 	return script_full_name;
     }
 
@@ -152,7 +143,6 @@ class JS_WScript extends Component {
     // ScriptName        https://msdn.microsoft.com/en-us/library/3faf1xkh(v=vs.84).aspx
     get scriptname () {
 	let script_name = this.context.ENVIRONMENT.ScriptName;
-	this.ee.emit("@WScript::ScriptName", script_name);
 	return script_name;
     }
 
@@ -166,26 +156,22 @@ class JS_WScript extends Component {
 
     // StdErr https://msdn.microsoft.com/en-us/library/hyez2k48(v=vs.84).aspx
     get stderr () {
-	this.ee.emit("@WScript::StdErr", "ERROR: NOT IMPLEMENTED!");
 	return null;
     }
 
     // StdIn https://msdn.microsoft.com/en-us/library/1y8934a7(v=vs.84).aspx
     get stdin () {
-	this.ee.emit("@WScript::StdIn", "ERROR: NOT IMPLEMENTED!");
 	return null;
     }
 
     // StdOut https://msdn.microsoft.com/en-us/library/c61dx86d(v=vs.84).aspx
     get stdout () {
-	this.ee.emit("@WScript::StdOut", "ERROR: NOT IMPLEMENTED!");
 	return null;
     }
 
     // Version https://msdn.microsoft.com/en-us/library/kaw07b53(v=vs.84).aspx
     get version () {
 	let version = this.context.ENVIRONMENT.Version;
-	this.ee.emit("@WScript.Version", version);
 	return version;
     }
 
@@ -203,13 +189,24 @@ class JS_WScript extends Component {
     // =======
     //
     connectobject () {
-	this.ee.emit("@WScript::ConnectObject", "ERROR: NOT IMPLEMENTED!");
     }
 
     createobject (type) {
 
         try {
             let instance = create_instance(this.context, type);
+
+            const apiobj = {
+                target:  "WScript",
+                id:      this.__id__,
+                hooked:  false,
+                type:    "constructor",
+                prop:    "new",
+                args:    [{ type: typeof type, value: type }],
+                return:  { instance: type, id: instance.__id__ }
+            };
+            this.context.emitter.emit(`runtime.api.constructor`, apiobj);
+
             return instance;
         }
         catch (e) {
@@ -229,13 +226,10 @@ class JS_WScript extends Component {
     }
 
     disconnectobject () {
-	this.ee.emit("@WScript::DisconnectObject", "ERROR: NOT IMPLEMENTED!");
     }
 
     echo (...args) {
 	let msg = args.join(" ");
-	this.ee.emit("@WScript::Echo", { msg: msg }, arguments);
-
         this.context.write_to_output_buf(msg);
 
 	if (this.context.output_behaviour === "repl") {
@@ -244,19 +238,16 @@ class JS_WScript extends Component {
     }
 
     getobject () {
-	this.ee.emit("@WScript::GetObject", "ERROR: NOT IMPLEMENTED!");
     }
 
     quit (exit_code) {
-	this.ee.emit("@WScript::Quit", exit_code);
-        this.context.shutdown(exit_code);
+        //this.context.shutdown(exit_code);
     }
 
     sleep (ms) {
         let old_time = this.context.epoch;
 	this.context.skew_time_ahead_by(ms);
         let new_time = this.context.epoch;
-	this.ee.emit("@WScript::Sleep", { old_time: old_time, new_time: new_time });
     }
 }
 
