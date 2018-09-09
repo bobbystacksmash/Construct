1a5aad16ed3c377aad658d8028bcd8ada9fdf6f4
bobbystacksmash <33038262+bobbystacksmash@users.noreply.github.com>
2018-09-09 21:30:52 +0100

    Added the Conditional Compilation MP module.

---
+++ src/metaprogramming/plugins/conditional_compilation.js
@@ -0,0 +1,20 @@
+// Conditional Compilation
+// =======================
+//
+// A module to handle JScript code which contains Conditional
+// Compilation elements, for example:
+//
+//  /*@cc_on
+//      @set (@version = 1)
+//      @if (@version == 1)
+//          alert('Version 1');
+//      @else
+//          alert('Not version 1');
+//      @end
+//  @*/
+//
+//
+// A Conditional Compilation (CC) block begins with:
+//
+//   @cc_on
+//
