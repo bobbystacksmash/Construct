1d3437c3b1f0dcedaf96d7227858b4f16a9e5b4d
bobbystacksmash <33038262+bobbystacksmash@users.noreply.github.com>
2018-10-18 19:58:41 +0100

    Moved .Environment inside the .Properties tests.

--- test/winapi/WshShell_test.js
+++ test/winapi/WshShell_test.js
@@ -120,32 +120,34 @@ describe("WshShell", () => {
                 assert.equal(wsh.CurrentDirectory, shortpath);
             });
         });
-    });
 
-    describe(".Environment", () => {
+        describe(".Environment", () => {
 
-        // WshShell.Environment returns a WshEnvironment collection.
-        // See `WshEnvironment_test.js` for tests specific to this
-        // class.
-        //
-        // This property has odd behaviour, acting as both a function
-        // and a property.  Two examples, both return a valid count:
-        //
-        //   - wsh.environment.count();
-        //   - wsh.environment("PROCESS").count();
-        //
-        it("should support fetching .Environment via either prop or method", () => {
+            // WshShell.Environment returns a WshEnvironment collection.
+            // See `WshEnvironment_test.js` for tests specific to this
+            // class.
+            //
+            // This property has odd behaviour, acting as both a function
+            // and a property.  Two examples, both return a valid count:
+            //
+            //   - wsh.environment.count();
+            //   - wsh.environment("PROCESS").count();
+            //
+            it("should support fetching .Environment via either prop or method", () => {
 
-            const wsh = new WshShell(ctx);
+                const wsh = new WshShell(ctx);
 
-            assert.doesNotThrow(() => wsh.environment.count());
-            assert.doesNotThrow(() => wsh.environment("PROCESS").count());
-            assert.equal(wsh.environment("PROCESS").count(), 2);
+                assert.doesNotThrow(() => wsh.environment.count());
+                assert.doesNotThrow(() => wsh.environment("PROCESS").count());
+                assert.equal(wsh.environment("PROCESS").count(), 2);
+
+                assert.doesNotThrow(() => wsh.environment("SYSTEM").count());
+                assert.equal(wsh.environment("SYSTEM").count(), 1);
+            });
         });
     });
 
-    /*
-    xdescribe(".Environment", () => {
+    /*xdescribe(".Environment", () => {
 
         // This property has odd behaviour, acting as both a function
         // and a property.  Two examples, both return a valid count:
