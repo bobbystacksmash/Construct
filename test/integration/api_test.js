d45c32c8ef5aaf0b22b65bf4e2a3f11fff52c474
bobbystacksmash <33038262+bobbystacksmash@users.noreply.github.com>
2018-11-08 21:12:20 +0000

    Added integration test file for working out the Construct public API.

---
+++ test/integration/api_test.js
@@ -0,0 +1,19 @@
+//    INTEGRATION TEST
+// Tests the Construct API
+
+const chai           = require("chai"),
+      chaiAsPromised = require("chai-as-promised"),
+      Construct      = require("../../index");
+
+chai.use(chaiAsPromised);
+const assert = chai.assert;
+
+describe("#Construct public API", () => {
+
+    describe("Loading a JScript program", () => {
+
+        it("should throw if the program contains a syntax error", () => {
+
+        });
+    });
+});
